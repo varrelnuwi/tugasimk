a0e1aa799f3b3617f44c6cda22d3bd425864b29d
RianHaryadi <rianharyadi89@gmail.com>
2024-11-12 15:14:35 +0700

    update

--- home/home.js
+++ home/home.js
@@ -1,3 +1,5 @@
 function toggleMenu() {
     document.querySelector('.navbar-container').classList.toggle('active');
 }
+
+document.getElementById('menu-icon').addEventListener('click', toggleMenu);
